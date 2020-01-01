b7c172bfaad23ce645b64b6e0682f33e2ef6ca33
TDAK <dbcgdhh@gmail.com>
2020-01-01 17:04:11 +0700

    Removes appId from electron config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
-        appId: "me.tdak.bi_finance_tool",
         productName: "Kế toán ti đắc",
         copyright: "Copyright © 2019 TDAK",
         extraFiles: ["src/assets/images/icon_32x32.ico"],
