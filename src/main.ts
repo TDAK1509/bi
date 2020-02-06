821aca2167cbbdbd5915b9a471c0f8f5d968513f
TDAK <dbcgdhh@gmail.com>
2020-02-06 20:53:35 +0700

    Add vue-select, update scripts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import router from "./router";
 import store from "./store";
 import Buefy from "buefy";
 import "./../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import vSelect from "vue-select";
 
 Vue.config.productionTip = false;
 Vue.use(Buefy, {
   defaultIconPack: "fas"
 });
+
+Vue.component("v-select", vSelect);
 
 new Vue({
   router,
