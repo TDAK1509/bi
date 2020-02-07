e554de44a7826c02a729bd3c88d902aa2e384166
TDAK <dbcgdhh@gmail.com>
2020-02-07 22:10:20 +0700

    Add apiClient.ts

---
+++ src/api/apis/apiClient.ts
@@ -0,0 +1,36 @@
+import { db } from "@/firebase";
+import { ApiRes } from "@/api/api-res";
+import { Client, Debt, ClientView } from "@/models/client";
+
+export default class ApiClient {
+  private db: firebase.firestore.Firestore;
+
+  constructor() {
+    this.db = db;
+  }
+
+  async addClient(client: Client): Promise<string> {
+    const docRef = await this.db
+      .collection(ApiRes.FirebaseCollection.CLIENTS)
+      .add(client);
+    return docRef.id;
+  }
+
+  async fetchClients(): Promise<ClientView[]> {
+    const clients: ClientView[] = [];
+
+    const docRef = await this.db.collection(ApiRes.FirebaseCollection.CLIENTS);
+    const querySnapshot = await docRef.get();
+
+    querySnapshot.forEach(doc => {
+      const data = <Client>doc.data();
+      const client: ClientView = {
+        id: doc.id.toString(),
+        ...data
+      };
+      clients.push(client);
+    });
+
+    return clients;
+  }
+}
