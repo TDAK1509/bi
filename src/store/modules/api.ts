562a15314c34ce431d9327f6fd06e4a1682856ef
TDAK <dbcgdhh@gmail.com>
2020-02-08 11:00:29 +0700

    Remove apiClient from module api

--- src/store/modules/api.ts
+++ src/store/modules/api.ts
@@ -1,12 +1,10 @@
 import ApiAuth from "@/api/apis/apiAuth";
-import ApiClient from "@/api/apis/apiClient";
 import ApiTransaction from "@/api/apis/apiTransaction";
 import ApiDebt from "@/api/apis/apiDebt";
 import ApiOptions from "@/api/apis/apiOptions";
 
 export class ApiState {
   auth = new ApiAuth();
-  client = new ApiClient();
   transaction = new ApiTransaction();
   debt = new ApiDebt();
   options = new ApiOptions();
