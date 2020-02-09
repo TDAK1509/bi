6bb9444dfdc7e5ce62cae8f4ececc6ed910bcd91
TDAK <dbcgdhh@gmail.com>
2020-02-09 15:21:28 +0700

    Structure search items

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,9 +1,66 @@
 <template>
   <div class="search">
-    <page-title>
-      <b-icon icon="search" />
-      TÌM KIẾM GIAO DỊCH
-    </page-title>
+    <page-title>TÌM GIAO DỊCH</page-title>
+
+    <div class="search__select-container">
+      <div class="search__select-item">
+        <b-radio v-model="searchCriteria" type="is-dark" native-value="id">
+          Mã giao dịch
+        </b-radio>
+
+        <b-input
+          class="search__select-item-input"
+          v-model="searchValue"
+          placeholder="Điền mã giao dịch"
+        ></b-input>
+      </div>
+
+      <div class="search__select-item">
+        <b-radio
+          v-model="searchCriteria"
+          type="is-dark"
+          native-value="client_name"
+        >
+          Tên khách hàng
+        </b-radio>
+      </div>
+
+      <div class="search__select-item">
+        <b-radio
+          v-model="searchCriteria"
+          type="is-dark"
+          native-value="seller_name"
+        >
+          Tên người bán
+        </b-radio>
+      </div>
+
+      <div class="search__select-item">
+        <b-radio
+          v-model="searchCriteria"
+          type="is-dark"
+          native-value="product_name"
+        >
+          Tên hàng hóa
+        </b-radio>
+      </div>
+
+      <div class="search__select-item">
+        <b-radio
+          v-model="searchCriteria"
+          type="is-dark"
+          native-value="payment_type"
+        >
+          Hình thức thanh toán
+        </b-radio>
+      </div>
+
+      <div class="search__select-item">
+        <b-radio v-model="searchCriteria" type="is-dark" native-value="amount">
+          Thành tiền
+        </b-radio>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +73,25 @@ import PageTitle from "@/components/PageTitle.vue";
     PageTitle
   }
 })
-export default class Search extends Vue {}
+export default class Search extends Vue {
+  searchCriteria: string = "id";
+  searchValue: string = "";
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search__select-container {
+  width: 500px;
+  margin: auto;
+}
+
+.search__select-item {
+  &:not(:first-child) {
+    margin-top: 20px;
+  }
+}
+
+.search__select-item-input {
+  margin-top: 10px;
+}
+</style>
