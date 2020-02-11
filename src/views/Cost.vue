437b4dc513c13c025cb9c97ed2fee4a561abb7c0
TDAK <dbcgdhh@gmail.com>
2020-02-11 19:02:30 +0700

    Init Cost view

---
+++ src/views/Cost.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cost">Cost</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Cost extends Vue {}
+</script>
+
+<style langg="scss" scoped></style>
