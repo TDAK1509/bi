fe0c3c7c6f96bfed1509e20e4446ec09179abd1a
TDAK <dbcgdhh@gmail.com>
2020-02-12 21:20:55 +0700

    Change options product to be Product type

--- src/models/helpers.ts
+++ src/models/helpers.ts
@@ -3,9 +3,15 @@ export type FilterType = "month" | "year" | "all";
 export interface SelectOptions {
   sellers: string[];
   transaction_types: string[];
-  product_names: string[];
+  product_names: string[] | Product[];
   payment_types: string[];
   clients: string[];
+}
+
+export interface Product {
+  name: string;
+  stock: number;
+  unit: string;
 }
 
 export interface ErrorMessage {
