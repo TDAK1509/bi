d86c287f6bc6945ae3e7871e939cc361436ff8cc
TDAK <dbcgdhh@gmail.com>
2020-02-13 20:46:13 +0700

    Style stock view

--- src/views/Stock.vue
+++ src/views/Stock.vue
@@ -3,13 +3,39 @@
     <page-title>QUẢN LÝ KHO</page-title>
 
     <div class="stock__product-container">
-      <b-field
-        v-for="(product, index) in productList"
-        :key="index + '-' + product.name"
+      <div class="stock__save-button-container">
+        <button class="stock__save-button">LƯU</button>
+      </div>
+
+      <b-table
+        paginated
+        per-page="10"
+        default-sort="date"
+        sort-icon="chevron-up"
+        :data="sortedProductList"
       >
-        <span>{{ product.name }}</span>
-        <b-numberinput :value="product.stock"></b-numberinput>
-      </b-field>
+        >
+        <template slot-scope="props">
+          <b-table-column field="name" label="Tên Sản Phẩm" searchable sortable>
+            {{ props.row.name }}
+          </b-table-column>
+
+          <b-table-column
+            field="stock"
+            label="Số Lượng Còn Lại"
+            sortable
+            numeric
+          >
+            <div class="stock__product-stock">
+              <b-numberinput
+                :value="props.row.stock"
+                controls-position="compact"
+                type="is-dark"
+              />
+            </div>
+          </b-table-column>
+        </template>
+      </b-table>
     </div>
 
     <b-loading :is-full-page="true" :active.sync="isLoading"></b-loading>
@@ -21,6 +47,8 @@ import { Vue, Component, Mixins } from "vue-property-decorator";
 import PageTitle from "@/components/PageTitle.vue";
 import ErrorHandling from "@/mixins/errorHandling";
 import Options from "@/mixins/options";
+import { Product } from "../models/helpers";
+import * as _ from "lodash";
 
 @Component({
   components: {
@@ -32,6 +60,11 @@ export default class Stock extends Mixins(ErrorHandling, Options) {
     return this.$store.state.cost.isFetchingCosts;
   }
 
+  get sortedProductList(): Product[] {
+    const productList = [...this.productList];
+    return _.sortBy(productList, ["name"]);
+  }
+
   mounted() {
     if (!this.$store.state.options.isFetchedOptions) {
       this.$store.dispatch("options/fetchOptions");
@@ -40,4 +73,37 @@ export default class Stock extends Mixins(ErrorHandling, Options) {
 }
 </script>
 
-<style langg="scss" scoped></style>
+<style lang="scss" scoped>
+.stock__product-container {
+  width: 550px;
+  margin: auto;
+}
+
+.stock__product-stock {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.stock__save-button-container {
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.stock__save-button {
+  border: 1px solid #000;
+  background: #000;
+  color: #fff;
+  padding: 10px 30px;
+  border-radius: 4px;
+  font-weight: 700;
+  letter-spacing: 2px;
+  cursor: pointer;
+  transition: 0.2s;
+
+  &:hover {
+    background: #fff;
+    color: #000;
+  }
+}
+</style>
